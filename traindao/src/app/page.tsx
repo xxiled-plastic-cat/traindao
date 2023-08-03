3f07751be81e38dcb3640c4ee78049c85352c56a
Xxiled <93330009+xxiled-plastic-cat@users.noreply.github.com>
2023-08-03 23:50:50 +0100

    yay first stuff!

---
+++ traindao/src/app/page.tsx
@@ -0,0 +1,142 @@
+'use client'
+
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import Bars3Icon from '@/Components/BarsIcon'
+import XMarkIcon from '@/Components/xmarkIcon'
+
+const navigation = [
+  { name: 'About', href: '/about' },
+  { name: 'Voting', href: 'https://xballot.net/traindao' },
+  { name: 'Discord', href: 'https://discord.gg/Tjb4Hsfk8h' },
+  { name: 'X', href: 'https://twitter.com/TrainDAO_algo' },
+]
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="bg-trainBG1 h-screen">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-12 w-auto rounded-full"
+                src="Choo.svg"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="text-xl font-semibold leading-6 text-gray-900">
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            
+          </div>
+        </nav>
+        <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          
+        </div>
+        <div className="mx-auto max-w-2xl sm:py-12 lg:py-14">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative  px-3 py-1 text-sm leading-6 text-gray-600 ">
+            <img
+                className=" w-auto"
+                src="TRAIN (1500 × 500px).png"
+                alt="#EAE164"
+              />              
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-8xl font-bold tracking-tight text-gray-900 sm:text-8xl">
+              ALL ABOARD!
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Join the most inclusive DAO around - learn about trains, blockchain, modelling and have fun doing it!
+            </p>
+            {/* <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div> */}
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  )
+}
